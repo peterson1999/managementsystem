f217844b4733a532d375fd6f563b8f3fa5bf61d7
peterson1999 <anjunieco@gmail.com>
2020-09-11 01:13:50 +0800

    dsafas

--- static/js/search.js
+++ static/js/search.js
@@ -1,6 +1,11 @@
 
             $(document).ready(function () {
-              $('#dtBasicExample').DataTable({  
+              var oTable= $('#dtBasicExample').DataTable({  
+                "oLanguage": {
+                  "sSearch": "Filter Data"
+                },
+                "iDisplayLength": -1,
+                "sPaginationType": "full_numbers",
                 dom: 'lBfrtip',
                   buttons: [
                   //{
@@ -11,8 +16,73 @@
                 'csvHtml5',
                 'pdfHtml5'
             ]});
+            // oTable.buttons().container().addClass('btn btn-danger');
+            Table(oTable);
 
               $('.dataTables_length').addClass('bs-select');
               });
 
 
+            function Table(  oTable) {
+              // var oTable = $('#dtBasicExample').DataTable({
+              //   "oLanguage": {
+              //     "sSearch": "Filter Data"
+              //   },
+              //   "iDisplayLength": -1,
+              //   "sPaginationType": "full_numbers",
+            
+              // });
+
+              $("#datepicker_from").datepicker({
+                showOn: "button",
+                buttonImage: "/static/resources/calendar.png",
+                buttonImageOnly: false,
+                "onSelect": function(date) {
+                  minDateFilter = new Date(date).getTime();
+                  oTable.draw();
+                }
+              }).keyup(function() {
+                minDateFilter = new Date(this.value).getTime();
+                oTable.draw();
+              });
+            
+              $("#datepicker_to").datepicker({
+                showOn: "button",
+                buttonImage: "/static/resources/calendar.png",
+                buttonImageOnly: false,
+                "onSelect": function(date) {
+                  maxDateFilter = new Date(date).getTime();
+                  oTable.draw();
+                }
+              }).keyup(function() {
+                maxDateFilter = new Date(this.value).getTime();
+                oTable.draw();
+              });
+            
+            }
+            
+            // Date range filter
+            minDateFilter = "";
+            maxDateFilter = "";
+            
+            $.fn.dataTableExt.afnFiltering.push(
+              function(oSettings, aData, iDataIndex) {
+                if (typeof aData._date == 'undefined') {
+                  aData._date = new Date(aData[0]).getTime();
+                }
+            
+                if (minDateFilter && !isNaN(minDateFilter)) {
+                  if (aData._date < minDateFilter) {
+                    return false;
+                  }
+                }
+            
+                if (maxDateFilter && !isNaN(maxDateFilter)) {
+                  if (aData._date > maxDateFilter) {
+                    return false;
+                  }
+                }
+            
+                return true;
+              }
+            );
