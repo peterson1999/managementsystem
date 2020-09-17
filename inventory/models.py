831a99e574d73142109aa451925c9146fd0719df
Wesley Ryan <wrdac@yahoo.com>
2020-09-17 13:09:28 +0800

    pull from productmodel branch

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+
+class Supplier (models.Model):
+
+    name= models.CharField(max_length=100)
+    address= models.CharField(max_length=100)
+class Product (models.Model):
+
+    regdate = models.DateField(default =datetime.now())
+    category = models.CharField(max_length=100)
+    brand =models.CharField(max_length=100)
+    name= models.CharField(max_length=100)
+    price= models.FloatField(max_length=11)
+    stock= models.IntegerField()
+    # supplier = models.ForeignKey(Supplier, null=False,blank= False, on_delete =  models.CASCADE, related_name="Supplier", default= 1)
+
+class Meta:
+    db_table = "Product"
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .forms import *
+from django.http import Http404
+from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
 # Create your views here.
 
 
@@ -8,4 +13,20 @@ class ProductRegisterView(View):
         return render(request, 'register-products.html')
 
     def post(self, request):
-        return render(request, 'home.html')
+        form = ProductForm(request.POST)
+
+        if form.is_valid():
+            category = request.POST.get("category")
+            brand = request.POST.get("brand")
+            name = request.POST.get("name")
+            price = request.POST.get("price")
+            stock = request.POST.get("stock")
+        
+            form = Product(category = category, brand=brand,name=name, price=price,stock=stock)
+            form.save()
+            return HttpResponse ('Product Saved!')
+
+        else:
+            print(form.errors)
+            return HttpResponse('not valid')
+        # return render(request, 'home.html')
